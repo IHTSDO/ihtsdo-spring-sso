663ecfe6351d742ca4979f7eb48fbcdecdbadf51
Kai Kewley <kaikewley@gmail.com>
2015-11-05 13:06:21 +0000

    Plain old application.

---
+++ src/main/java/org/ihtsdo/example/ims/HelloController.java
@@ -0,0 +1,14 @@
+package org.ihtsdo.example.ims;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Hello";
+	}
+
+}
