82099538cea99929ab051d24cfaf8a024d2dba17
Kai Kewley <kaikewley@gmail.com>
2015-11-05 14:19:37 +0000

    Null check when user not logged in and authentication disabled.

--- src/main/java/org/ihtsdo/example/ims/ControllerHelper.java
+++ src/main/java/org/ihtsdo/example/ims/ControllerHelper.java
@@ -1,15 +1,28 @@
 package org.ihtsdo.example.ims;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class ControllerHelper {
 
 	public static UserDetails getUserDetails() {
-		return (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		final SecurityContext context = SecurityContextHolder.getContext();
+		if (context != null) {
+			final Authentication authentication = context.getAuthentication();
+			if (authentication != null) {
+				return (UserDetails) authentication.getPrincipal();
+			}
+		}
+		return null;
 	}
 
 	public static String getUsername() {
-		return getUserDetails().getUsername();
+		final UserDetails userDetails = getUserDetails();
+		if (userDetails != null) {
+			return userDetails.getUsername();
+		}
+		return null;
 	}
 }
