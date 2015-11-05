26cab2fd1e77b52a137e878a559c4eae0f6e7680
Kai Kewley <kaikewley@gmail.com>
2015-11-05 13:54:23 +0000

    Add IMS integration.

--- src/main/java/org/ihtsdo/example/ims/Application.java
+++ src/main/java/org/ihtsdo/example/ims/Application.java
@@ -1,9 +1,14 @@
 package org.ihtsdo.example.ims;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("security-context.xml")
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/org/ihtsdo/example/ims/ControllerHelper.java
@@ -0,0 +1,15 @@
+package org.ihtsdo.example.ims;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class ControllerHelper {
+
+	public static UserDetails getUserDetails() {
+		return (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	}
+
+	public static String getUsername() {
+		return getUserDetails().getUsername();
+	}
+}
