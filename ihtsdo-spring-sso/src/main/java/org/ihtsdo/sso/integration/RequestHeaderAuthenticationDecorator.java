9926236526faa02dcf0c93401640db8df34fd099
Elena Ilyukhina <eil@ihtsdo.org>
2022-02-15 16:15:04 +0000

    FRI-225 - Refined RequestHeaderAuthenticationDecorator

--- ihtsdo-spring-sso/src/main/java/org/ihtsdo/sso/integration/RequestHeaderAuthenticationDecorator.java
+++ ihtsdo-spring-sso/src/main/java/org/ihtsdo/sso/integration/RequestHeaderAuthenticationDecorator.java
@@ -21,48 +21,44 @@ public class RequestHeaderAuthenticationDecorator extends OncePerRequestFilter {
 	private static final String TOKEN = "X-AUTH-token";
 
 	@Override
-	protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain filterChain)
-			throws ServletException, IOException {
-
+	protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain filterChain) throws ServletException, IOException {
 		Authentication authentication = SecurityUtil.getAuthentication();
+		String authenticationToken = SecurityUtil.getAuthenticationToken();
 
-		if (authentication instanceof PreAuthenticatedAuthenticationToken) {
-
-			String authenticationToken = SecurityUtil.getAuthenticationToken();
-
-			if (authenticationToken != null && authenticationToken.equals(request.getHeader(TOKEN))) {
-				// Security context already holds the correct authorities - do nothing and return
-				filterChain.doFilter(request, response);
-				return;
-			} else {
-				// Security context holds outdated authorities - invalidate current session and continue
-				HttpSession session = request.getSession(false);
-				if (session != null) {
-					session.invalidate();
-				}
+		if (authenticationToken == null || !authenticationToken.equals(request.getHeader(TOKEN))) {
+			// Clear context and invalidate session (if available)
+			SecurityContextHolder.clearContext();
+			HttpSession session = request.getSession(false);
+			if (session != null) {
+				session.invalidate();
+				request.getSession();
 			}
+
+			// Create new authentication with username, security token and authorities from request header
+			final AbstractAuthenticationToken decoratedAuthentication = new PreAuthenticatedAuthenticationToken(
+					request.getHeader(USERNAME),
+					request.getHeader(TOKEN),
+					AuthorityUtils.commaSeparatedStringToAuthorityList(request.getHeader(ROLES)));
+
+			// Pass through existing authentication details
+			decoratedAuthentication.setDetails(authentication.getDetails());
+
+			SecurityContextHolder.getContext().setAuthentication(decoratedAuthentication);
 		}
 
-		// Create authentication with username and security token from headers
-		final AbstractAuthenticationToken decoratedAuthentication = new PreAuthenticatedAuthenticationToken(
-				request.getHeader(USERNAME),
-				request.getHeader(TOKEN),
-				AuthorityUtils.commaSeparatedStringToAuthorityList(request.getHeader(ROLES)));
-
-		// Pass through existing details object
-		decoratedAuthentication.setDetails(authentication.getDetails());
-
-		SecurityContextHolder.getContext().setAuthentication(decoratedAuthentication);
 		filterChain.doFilter(request, response);
 
-		// Clear context from this thread when request complete
+		// Clear context from this thread when request completes
+		// Not needed here if this filter is registered within Spring Security filter chain (as it probably should) since the chain clears the context
 		SecurityContextHolder.clearContext();
 	}
 
 	@Override
 	protected boolean shouldNotFilter(final HttpServletRequest request) throws ServletException {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		return authentication == null || !authentication.isAuthenticated() || request.getHeader(USERNAME) == null || request.getHeader(ROLES) == null;
+		Authentication authentication = SecurityUtil.getAuthentication();
+		return authentication == null || !authentication.isAuthenticated() ||
+				request.getHeader(USERNAME) == null ||
+				request.getHeader(TOKEN) == null ||
+				request.getHeader(ROLES) == null;
 	}
-
 }
