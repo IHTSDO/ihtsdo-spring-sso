cb01354b6742968d0d1931d646f5a6d5d8fd32b0
Kai Kewley <kaikewley@gmail.com>
2023-03-30 15:46:44 +0100

    PIP-248 Install maven bom with library upgrades.

--- ihtsdo-spring-sso-example/src/main/java/org/ihtsdo/sso/example/Application.java
+++ ihtsdo-spring-sso-example/src/main/java/org/ihtsdo/sso/example/Application.java
@@ -1,14 +1,11 @@
 package org.ihtsdo.sso.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource("security-context.xml")
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
